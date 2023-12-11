7b5f18a6b1d997b2c89f42d47c982aa444e43ce7
alekseylavrishin <78001914+alekseylavrishin@users.noreply.github.com>
2023-12-11 08:01:21 -0500

    Fixed DB Connection String

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import FollowsRoutes from "./follows/routes.js";
 import FeaturesRoutes from "./features/routes.js";
 
 //mongoose.connect("mongodb://127.0.0.1:27017/cs-5610-final-project");
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/cs-5610-final-project';
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
